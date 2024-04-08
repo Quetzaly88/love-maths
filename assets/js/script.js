84a67d36b2745c21d0e60affe33a2484e9dd2998
Andrea Quetzaly Nilsson <andreaycoral@icloud.com>
2024-04-08 12:54:44 +0000

    Set up structure of game functions

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function calculateCorrectAnswer() {
+
+}
+
+function incrementScore() {
+
+}
+
+function incrementWrongAnswer() {
+
+}
+
+function displayAddition() {
+
+}
+
+function displaySubtractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
+    
+}
